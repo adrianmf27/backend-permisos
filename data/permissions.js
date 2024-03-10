342898f86ce1d07f913f25e819a77437c077fc1a
Adrián Martínez Fuentes <UO295454@uniovi.es>
2024-03-10 14:28:12 +0100

    Initial struct

---
+++ data/permissions.js
@@ -0,0 +1,6 @@
+let permissions = [
+    {id: 1, text: "Hacer una compra", approbedBy: [], userID: 1},
+    {id: 1, text: "Hacer una compra", approbedBy: [], userID: 1}
+]
+
+module.exports = permissions
