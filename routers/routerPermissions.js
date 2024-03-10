6f295955804a3e25daade8890d26ef28d478aa34
Adrián Martínez Fuentes <UO295454@uniovi.es>
2024-03-10 21:20:39 +0100

    New changes

--- routers/routerPermissions.js
+++ routers/routerPermissions.js
@@ -14,6 +14,19 @@ routerPermissions.post("/", (req, res) => {
     let text = req.body.text
     let userId = req.body.userId
     let lastID = permissions[permissions.length - 1].id
+    let errors = []
+
+    if(text == undefined){
+        errors.push("No text in the body")
+    }
+
+    if(userId == undefined){
+        errors.push("No userId in the body")
+    }
+
+    if(errors.length >= 1){
+        return res.status(400).json({errors: errors})
+    }
 
     permissions.push({
         id: lastID + 1, 
---
+++ routers/routerUsers.js
@@ -0,0 +1,45 @@
+const express = require("express")
+const routerUsers = express.Router()
+let users = require("../data/users")
+
+routerUsers.get("/", (req, res) => {
+    res.json(users.map(u => { return { id: u.id, email:u.email } } ))
+})
+
+routerPermissions.post("/", (req, res) => {
+    let text = req.body.text
+    let userEmail = req.body.userEmail
+    let userPassword = req.body.userPassword
+
+    // validate candidate users
+    let possibleUser = users.filter(u => u.email == userEmail && u.password == userPassword)
+    if(possibleUser.length == 0){
+        return res.status(401).json({errors: "Not authorized"})
+    }
+
+    let errors = []
+    if(text == undefined){
+        errors.push("No text in the body")
+    }
+
+    if(possibleUser[0].id == undefined){
+        errors.push("No userId in the body")
+    }
+
+    if(errors.length >= 1){
+        return res.status(400).json({errors: errors})
+    }
+
+    permissions.push({
+        id: lastID + 1, 
+        text: text, 
+        approbedBy: [], 
+        userId: possibleUser[0].id
+    })
+
+    /* Se devuelve un obj json con el último elemento añadido */
+    res.json({id: lastID + 1}) 
+})
+
+
+module.exports = routerUsers
